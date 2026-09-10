87506ac1f7a322ce293d7a1231c4adf2622c3611
akmniazulalam <niazulalam097@gmail.com>
2026-09-11 01:49:24 +0600

    chore: implement PrismaService for database connection management

---
+++ apps/api/src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client.js';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  constructor(configService: ConfigService) {
+    const databaseUrl = configService.getOrThrow<string>('DATABASE_URL');
+    const adapter = new PrismaPg({ connectionString: databaseUrl });
+
+    super({ adapter });
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
